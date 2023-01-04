8c62edee504d26c634fd40913cf40a2933445389
Airme1 <emmanuelochoja@yahoo.com>
2023-01-04 02:02:01 +0100

    2-args.py space removal

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 import sys
 if len(sys.argv) == 2:
     print("1 argument:")
-    print("1:", sys.argv[1])
+    print("1:" + sys.argv[1])
 elif len(sys.argv) == 1:
     print("0 arguments.")
 else:
     print(len(sys.argv) - 1 , "arguments:")
     for i in range(1,len(sys.argv)):
-        print(i, ":", sys.argv[i])
+        print(i + ":", sys.argv[i])
