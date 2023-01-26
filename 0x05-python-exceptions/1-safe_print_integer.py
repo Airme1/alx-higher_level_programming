5bc550fa4952f9dd7f84c9a6bc82961151adfbbd
Airme1 <emmanuelochoja@yahoo.com>
2023-01-26 16:08:56 +0100

    1-safe_print_integer.py shabang

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
+    except ValueError:
         print("{}".format(value))
         return False
