2bdc452bcf9bfdacf3522c6bfd7a4fa8801fda80
Airme1 <emmanuelochoja@yahoo.com>
2023-03-09 23:53:32 +0100

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ import calculator_1
 a = 10
 b = 5
 
-sumd = add(a, b)
-subd = sub(a, b)
-tim = mul(a, b)
-divd = div(a, b)
-
-print("{:d} + {:d} = {:d}".format(a,b,sumd))
-print("{:d} - {:d} = {:d}".format(a,b,subd))
-print("{:d} * {:d} = {:d}".format(a,b,tim))
-print("{:d} / {:d} = {:d}".format(a,b,divd))
+print("{:d} + {:d} = {:d}".format(a,b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a,b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a,b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a,b, div(a, b)))
