db316e50b9283806524e0580ecb0b2ed9bf6f4c4
Airme1 <emmanuelochoja@yahoo.com>
2023-04-06 02:50:56 +0100

    little mod to 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,10 +4,9 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
-    # property decorator for getter fxn- retrieve value
     @property
     def width(self):
         """Gets the value of the (private) width"""
@@ -16,9 +15,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the value of the (private) width"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,7 +29,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the value of height"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("height must be >= 0")
