f52e47a8c2af4a2fbd622a49d2b5665d8320a037
Airme1 <emmanuelochoja@yahoo.com>
2023-04-12 18:29:30 +0100

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Fxn to read a text file UTF8 and print to stdout
+"""
+
+
+def read_file(filename=""):
+    """Fxn to receive and read file"""
+    with open(filename, 'r', encoding="utf-8") as object_file:
+        read_data = object_file.read()
+    print(read_data)
