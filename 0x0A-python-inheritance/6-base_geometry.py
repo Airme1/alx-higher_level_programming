923b10cf96559a5b5aa0cebf6ad52e9ddd2b13f0
Airme1 <emmanuelochoja@yahoo.com>
2023-04-12 05:50:42 +0100

    adjust comment

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,5 +5,8 @@ Raise exception: Area is not implemented
 
 
 class BaseGeometry:
+    """class has just a method, no instantiation"""
+
     def area(self):
+        """fxn to raise exception"""
         raise Exception("area() is not implemented")
