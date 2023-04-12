389101868802c12f8a2af1ef56a9f5fbc059208a
Airme1 <emmanuelochoja@yahoo.com>
2023-04-12 23:10:46 +0100

    within [with]

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """Fxn to receive and read file"""
     with open(filename, 'r', encoding="utf-8") as object_file:
         read_data = object_file.read()
-    print(read_data)
+        print(read_data)
