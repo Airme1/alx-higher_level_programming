502c5b2b9d267f61f44afc6798682d926e8f59bd
Airme1 <emmanuelochoja@yahoo.com>
2023-04-13 04:14:31 +0100

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json 
+"""
+Function to return JSON representation of object
+"""
+
+
+def to_json_string(my_obj):
+    """json rep of my_obj calculated and sent to x"""
+    return json.dumps(my_obj)
