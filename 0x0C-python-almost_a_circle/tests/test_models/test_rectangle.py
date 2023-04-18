52efd1581b62b2b1d7691630ecef58094d90481d
Airme1 <emmanuelochoja@yahoo.com>
2023-04-18 04:13:53 +0100

    test_Aggain

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
 """Rectangle testing"""
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """Test Rectangle class"""
+
+    def test_width(self):
+        """Test rectangle width"""
+        rec = Rectangle(8,9)
+        self.assertEqual(8, self.rec.width)
+
+    def test_height(self):
+        """Test rectangle height"""
+        rec = Rectangle(8,9)
+        self.assertEqual(9, self.rec.height)
+
+     def test_x(self):
+        """Test rectangle x val"""
+        rec = Rectangle()
+        rec.x = 10
+        self.assertEqual(10, self.rec.x )
+
+     def test_y(self):
+        """Test rectangle y val"""
+        rec = Rectangle(8,9)
+        self.assertEqual(8, self.rec.width)
+
