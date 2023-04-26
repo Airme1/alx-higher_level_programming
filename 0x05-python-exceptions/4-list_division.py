83b29416a274fe48ea4e9b9b1fe400fcffb98a1a
Airme1 <emmanuelochoja@yahoo.com>
2023-04-26 13:16:55 +0100

    dealing with list_lenght parameter

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,13 +2,6 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    L1 = len(my_list_1)
-    L2 = len(my_list_2)
-    if L1 > L2:
-        list_length = L1
-    else:
-        list_length = L2
-
     for num in range(list_length):
         try:
             div = my_list_1[num] / my_list_2[num]
