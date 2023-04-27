ec2609abaf9e803423bc2bc547ab1409c9bd4102
Airme1 <emmanuelochoja@yahoo.com>
2023-04-27 03:12:15 +0100

    make private at end

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@
 
 
 class Square():
+    """instantiation of Square"""
+
     def __init__(self, size=0):
-        self.__size = size
-        try:
-            type(self.__size) == int
-        except TypeError:
-            print("size must be an integer")
-        if self.__size < 0:
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
