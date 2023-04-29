d1337e27f9fa9684fbf274626ab9eee449cc4f13
Airme1 <emmanuelochoja@yahoo.com>
2023-04-29 20:09:37 +0100

    pycodestyle commpliance

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,7 @@ class Square():
     """A class to find area of a square with a private instance attribute"""
     def __init__(self, size=0):
         """instantiation of class"""
-        
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
