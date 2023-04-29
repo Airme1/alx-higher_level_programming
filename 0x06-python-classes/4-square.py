6fe042d4977cc555430c6224f6a9ce5af7b2288f
Airme1 <emmanuelochoja@yahoo.com>
2023-04-29 23:19:13 +0100

    add condition in instantiation

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,6 +3,10 @@ class Square:
     """Class to use setter and getter"""
     def __init__(self, size=0):
         """instantiate class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
