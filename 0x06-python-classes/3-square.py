52c27cfb47684fe0d759611afe72ccecfabd686a
Airme1 <emmanuelochoja@yahoo.com>
2023-04-29 20:07:36 +0100

    indentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,10 +3,9 @@ class Square():
     """A class to find area of a square with a private instance attribute"""
     def __init__(self, size=0):
         """instantiation of class"""
-        try:
-            type(size) == int
-        except TypeError:
-            print("size must be an integer")
+        
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
         self.__size = size
