4ed2874492b03f13e14223beed656a096ad14a7f
Airme1 <emmanuelochoja@yahoo.com>
2023-05-18 05:37:09 +0100

    count all and mod comment

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
---Display number of records with id=89 in first_table of hbtn_0c_0
-SELECT COUNT(id)
+-- Display number of records with id=89 in first_table of hbtn_0c_0
+SELECT COUNT(*)
 FROM first_table
 WHERE id = 89;
