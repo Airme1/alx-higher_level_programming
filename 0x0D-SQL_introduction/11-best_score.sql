c4fcf10e432eff08ad8dd2b1cfe5245d7f7eb916
Airme1 <emmanuelochoja@yahoo.com>
2023-05-18 05:52:40 +0100

    changed from * to score and name

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- list all records with score >= 10
 -- in the second_table of hbtn_0c_0 database
-SELECT * FROM second_table
+SELECT score, name FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
