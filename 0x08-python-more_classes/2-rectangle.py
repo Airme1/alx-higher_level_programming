c6c18e8d917880017632bb6f15744a8dd5b05e43
Airme1 <emmanuelochoja@yahoo.com>
2023-07-08 00:31:55 +0100

    mod indentation

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,25 +17,25 @@ class Rectangle:
     @width.setter
     def width(self, value):
          """setter method"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+         if type(value) is not int:
+             raise TypeError("width must be an integer")
+         if value < 0:
+             raise ValueError("width must be >= 0")
+         self.__width = value
  
     @property
     def height(self):
-         """Using @decorator for height getter"""
+        """Using @decorator for height getter"""
         return self.__height
 
     @height.setter    
     def height(self, value):
          """setter method for height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+         if type(value) is not int:
+             raise TypeError("height must be an integer")
+         if value < 0:
+             raise ValueError("height must be >= 0")
+         self.__height = value
 
     def area(self):
         """method that returns area of the rectangle"""
