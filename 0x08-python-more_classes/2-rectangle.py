93ce6240e2871e567797dd7e2242296c7292ef90
Airme1 <emmanuelochoja@yahoo.com>
2023-07-08 00:23:39 +0100

    little mods

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,29 +5,32 @@
 class Rectangle:
     """Instatiation of rectangle"""
     def __init__(self, width=0, height=0):
+        """setting parameters"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter method"""
         return self.__width
-
-    """setter method"""
+ 
     @width.setter
     def width(self, value):
+         """setter method"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    """Using @decorator for height getter"""
+ 
     @property
     def height(self):
+         """Using @decorator for height getter"""
         return self.__height
 
-    """setter method for height"""
+    @height.setter    
     def height(self, value):
+         """setter method for height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
