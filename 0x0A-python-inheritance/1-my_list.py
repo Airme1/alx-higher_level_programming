0c8944b37f1078765f67dae48c28354d0290e84e
Airme1 <emmanuelochoja@yahoo.com>
2023-07-13 16:23:14 +0100

    entire mod

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
+"""module to inherit from list class"""
+
 
 class MyList(list):
+    """this class inherits list"""
     def print_sorted(self):
-        usr_list = list(eval(repr(self)))
-        usr_list.sort()
-        print(usr_list)
-
-    """Exception handling fxn"""
-    def excptn(self, num):
-        if type(num) is not type(int):
-            raise ValueError("Only integers permited in list")
-        super().append(item)
+        """method to sort list"""
+        print(sorted(self))
