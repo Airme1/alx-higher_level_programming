e91d34f62ae5d16ad687e31acf046ef489b96595
Airme1 <emmanuelochoja@yahoo.com>
2023-07-13 17:01:03 +0100

    change to issubclass()

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ indirectly from a class returns either True/False
 
 def inherits_from(obj, a_class):
     """Fxn return true is obj is instance of class"""
-    return isinstance(obj, a_class)
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
