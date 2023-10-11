8f180e675b16f0bbb847e4d96f045f90828152cb
Airme1 <emmanuelochoja@yahoo.com>
2023-10-11 02:56:11 +0100

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import json
 """
 returns dictionary description with simple data structure
 """
 
+
+import json
 
 def class_to_json(obj):
     """returns dictionary of object"""
